de02854909253882c972136d22bdf31ca422589f
Temitech1 <temidayoalabi159@gmail.com>
2024-08-02 01:19:07 +0200

    lockboxes challenging

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = set([0])  # Set of unlocked boxes, start with box 0
+    keys = set(boxes[0])  # Set of available keys, start with keys in box 0
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
